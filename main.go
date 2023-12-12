21676ad037afe9590c750018d636d9ae9b32906b
IceBearG <65737922+IceBeerG@users.noreply.github.com>
2023-12-12 23:36:39 +0400

    Add files via upload

--- main.go
+++ main.go
@@ -3,104 +3,54 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
+	"io/ioutil"
 )
 
-type Node struct {
+type Item struct {
 	ID       int    `json:"id"`
 	Text     string `json:"Text"`
 	Min      string `json:"Min"`
 	Value    string `json:"Value"`
 	Max      string `json:"Max"`
 	ImageURL string `json:"ImageURL"`
-	Children []Node `json:"Children"`
+	Children []Item `json:"Children"`
 }
 
-func main() {
-	url := "http://localhost:8085/data.json"
-	resp, err := http.Get(url)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-	tempCPU1, tempCPU2 := getTemp(body, "cpu")
-	tempGPU1, tempGPU2 := getTemp(body, "gpu")
-	if tempCPU1 != "" {
-		fmt.Println("Температура CPU = ", tempCPU1)
-	} else if tempCPU2 != "" {
-		fmt.Println("Температура CPU = ", tempCPU2)
-	}
-
-	if tempGPU1 != "" {
-		fmt.Println("Температура GPU = ", tempGPU1)
-	}
-	if tempGPU2 != "" {
-		fmt.Println("Температура HotSpot = ", tempGPU2)
-	}
-
-	var a string
-	fmt.Println("ожидание ввода")
-	fmt.Scan(&a)
-}
-
-func getTemp(body []byte, xpu string) (value1, value2 string) {
-
-	var root Node
-	var text1, text2, text3 string
-	err := json.Unmarshal(body, &root)
-	if err != nil {
-		panic(err)
-	}
-
-	value1, value2 = "", ""
-
-	if xpu == "cpu" {
-		text1 = "images_icon/cpu.png"
-		text2 = "Core (Tctl/Tdie)"
-		text3 = "CPU Package"
-	} else if xpu == "gpu" {
-		text1 = "images_icon/nvidia.png"
-		text2 = "GPU Core"
-		text3 = "GPU Hot Spot"
-	}
-
-	// Перебор первого уровня
-	for _, child := range root.Children {
-		// Перебор второго уровня
-		for _, subChild := range child.Children {
-			// fmt.Println("child.Children - ", subChild.ImageURL)
-			if subChild.ImageURL == text1 {
-				// Перебор третьего уровня
-				for _, subSubChild := range subChild.Children {
-					// fmt.Println("subSubChild.Children - ", subSubChild.ImageURL)
-
-					if subSubChild.Text == "Temperatures" {
-
-						// Перебор четвертого уровня
-						for _, subSubSubChild := range subSubChild.Children {
-							// fmt.Println("subSubSubChild.Text - ", subSubSubChild.Text)
-
-							if subSubSubChild.Text == text2 {
-								// fmt.Println("Значение поля Value:", subSubSubChild.Value)
-								value1 = subSubSubChild.Value
-								// return value1
-							}
-							if subSubSubChild.Text == text3 {
-								// fmt.Println("Значение поля Value:", subSubSubChild.Value)
-								value2 = subSubSubChild.Value
-								// return value2
-							}
+func findValue(items []Item) string {
+	for _, item := range items {
+		if item.ImageURL == "images_icon/cpu.png" {
+			for _, child := range item.Children {
+				if child.Text == "Temperatures" {
+					for _, grandchild := range child.Children {
+						if grandchild.Text == "Core (Tctl/Tdie)" {
+							return grandchild.Value
 						}
 					}
 				}
 			}
 		}
 	}
-	return value1, value2
+
+	return ""
+}
+
+func main() {
+	// Чтение файла
+	fileContent, err := ioutil.ReadFile("data-4070ti.json")
+	if err != nil {
+		fmt.Println("Ошибка при чтении файла:", err)
+		return
+	}
+
+	// Декодирование JSON
+	var items []Item
+	err = json.Unmarshal(fileContent, &items)
+	if err != nil {
+		fmt.Println("Ошибка при декодировании JSON:", err)
+		return
+	}
+
+	// Поиск значения
+	value := findValue(items)
+	fmt.Println("Значение поля Value:", value)
 }
